c9440e8d6ff00d12515210db234794ae44470c0f
sky-kt <skyler.kt@protonmail.com>
2021-08-04 11:08:10 -0700

    change gameboard generation

--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
-const playerBoard = document.getElementById('playerBoard')
-const computerBoard = document.getElementById('computerBoard');
+const humanBoard = document.getElementById('humanBoard')
+const robotBoard = document.getElementById('robotBoard')
+const numberRows = document.querySelectorAll('.numberRow')
+const letterRows = document.querySelectorAll('.letterRow');
 
-// set up boards
+// set up gameooards
 (() => {
-  for (let idx = 0; idx < 121; idx++) {
-    const tile = document.createElement('div')
-    if (idx === 0) {
-      tile.style.backgroundColor = '#dfdfdf'
-    } else if (idx >= 0 && idx <= 10) {
-      const letter = String.fromCharCode(idx + 64)
-      tile.appendChild(document.createTextNode(letter))
-      tile.classList.add('letterTile')
-    } else if (idx % 11 === 0) {
-      tile.appendChild(document.createTextNode(idx / 11))
-      tile.classList.add('numberTile')
-    } else {
+  for (let idx = 0; idx < 10; idx++) {
+    const boardRow = document.createElement('div')
+    boardRow.classList.add('boardRow')
+    for (let idx = 0; idx < 10; idx++) {
+      const tile = document.createElement('div')
       tile.classList.add('boardTile')
+      boardRow.appendChild(tile)
     }
+    humanBoard.appendChild(boardRow)
+    robotBoard.appendChild(boardRow.cloneNode(true))
+  }
+})();
 
-    const displayTile = (tile) => {
-      console.log(tile)
-    }
+(() => {
+  for (let idx = 0; idx < 10; idx++) {
+    numberRows.forEach(numberRow => {
+      const numberTile = document.createElement('div')
+      numberTile.classList.add('numberTile')
+      numberTile.appendChild(document.createTextNode(idx + 1))
+      numberRow.appendChild(numberTile)
+    })
+  }
+})();
 
-    const clonedTile = tile.cloneNode(true)
-    tile.addEventListener('click', () => displayTile(tile))
-    clonedTile.addEventListener('click', () => displayTile(clonedTile))
-
-    playerBoard.appendChild(tile)
-    computerBoard.appendChild(clonedTile)
+(() => {
+  for (let idx = 0; idx < 10; idx++) {
+    letterRows.forEach(letterRow => {
+      const letterTile = document.createElement('div')
+      letterTile.classList.add('letterTile')
+      const letter = String.fromCharCode(idx + 65)
+      letterTile.appendChild(document.createTextNode(letter))
+      letterRow.appendChild(letterTile)
+    })
   }
 })()
