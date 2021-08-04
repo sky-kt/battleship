467051eace913f9a07aa0b836d8236b3c9aa2435
sky-kt <skyler.kt@protonmail.com>
2021-08-03 19:17:03 -0700

    add index.js

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable*/
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
